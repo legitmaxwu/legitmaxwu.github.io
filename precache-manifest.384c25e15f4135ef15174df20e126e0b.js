5d59cd4cd656ceea7e340ce0e8a0a5a54161c8e5
Max Wu <chengmaxwu@gmail.com>
2019-12-27 02:10:17 -0800

    Updates

---
+++ precache-manifest.384c25e15f4135ef15174df20e126e0b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8404b62b7fd21e7b6516affb2e707e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3559a0a82fb8cfc49846",
+    "url": "/static/css/main.2e2a1953.chunk.css"
+  },
+  {
+    "revision": "cf26d2ca251546ebd52b",
+    "url": "/static/js/2.a0f314b0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.a0f314b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3559a0a82fb8cfc49846",
+    "url": "/static/js/main.3a4eb4af.chunk.js"
+  },
+  {
+    "revision": "fd103d41066edfe2edd7",
+    "url": "/static/js/runtime-main.06363229.js"
+  },
+  {
+    "revision": "5e36980c8f13599f2723df327651abfa",
+    "url": "/static/media/darkLeaves.5e36980c.png"
+  },
+  {
+    "revision": "c415e911c7b61675d0a85e44df7d9822",
+    "url": "/static/media/giraffe.c415e911.jpg"
+  },
+  {
+    "revision": "f94a4dd80e88299543a4ab20418f4b61",
+    "url": "/static/media/sidewalk.f94a4dd8.jpg"
+  }
+]);
