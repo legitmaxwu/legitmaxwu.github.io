f822adc7a9ff1d763f0a0748137fa518cea98ff8
Max Wu <chengmaxwu@gmail.com>
2019-12-27 03:10:10 -0800

    Updates

---
+++ precache-manifest.eafa8168169c7d5eeb0132c2373ba86f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8882b261bd0e5dff78af3e7a9545a07c",
+    "url": "/legitmaxwu.github.io/index.html"
+  },
+  {
+    "revision": "a50e931a6c9fc8f0879b",
+    "url": "/legitmaxwu.github.io/static/css/main.593eb56d.chunk.css"
+  },
+  {
+    "revision": "50ad4a6a3bb49293f825",
+    "url": "/legitmaxwu.github.io/static/js/2.f9c5b44d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/legitmaxwu.github.io/static/js/2.f9c5b44d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a50e931a6c9fc8f0879b",
+    "url": "/legitmaxwu.github.io/static/js/main.20e9ebb3.chunk.js"
+  },
+  {
+    "revision": "13f4eeb117ecd2d10e31",
+    "url": "/legitmaxwu.github.io/static/js/runtime-main.29ac6749.js"
+  },
+  {
+    "revision": "5e36980c8f13599f2723df327651abfa",
+    "url": "/legitmaxwu.github.io/static/media/darkLeaves.5e36980c.png"
+  },
+  {
+    "revision": "c415e911c7b61675d0a85e44df7d9822",
+    "url": "/legitmaxwu.github.io/static/media/giraffe.c415e911.jpg"
+  },
+  {
+    "revision": "f94a4dd80e88299543a4ab20418f4b61",
+    "url": "/legitmaxwu.github.io/static/media/sidewalk.f94a4dd8.jpg"
+  }
+]);
