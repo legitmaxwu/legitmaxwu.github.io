320cbf84ff2c8f374749e34ee55b9fcb21240deb
Max Wu <chengmaxwu@gmail.com>
2019-12-28 01:26:35 -0800

    added some more routing and art page

--- src/pages/Landing/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "react-tilt" {
-  const Tilt: any;
-  export default Tilt;
-}
--- src/shared/config.ts
+++
@@ -1,6 +0,0 @@
-export const tabletMediaQuery = "@media (max-width: 1000px)";
-export const mobileMediaQuery = "@media (max-width: 700px)";
-
-export const colors = {
-  teal: "#008080"
-};
